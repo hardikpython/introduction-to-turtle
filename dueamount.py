fcfd9ef392fb9f7b119ef0df926de089ba564c44
hardikpython <pk646155@gmail.com>
2025-04-01 16:19:14 +0530

    dueamount

---
+++ dueamount.py
@@ -0,0 +1,11 @@
+# Amount paid by Vishal
+amount_paid = 5.00
+
+# Total bill
+total_bill = 2.50
+
+# Calculate the change to be returned
+change = amount_paid - total_bill
+
+# Display the change
+print(f"The shopkeeper should return ${change:.2f} to Vishal.")
